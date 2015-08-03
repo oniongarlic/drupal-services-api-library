984517562987c6fbdf8bde1bd5d90dd7e7266b51
Kaj-Michael Lang <milang@tal.org>
2015-08-03 18:05:01 +0300

    More file upload and node field attach testing

--- test-file.php
+++ test-file.php
@@ -3,6 +3,9 @@
 require_once('DrupalServiceAPIClient.class.php');
 
 $c=new DrupalServiceAPIClient('http://drupal.ubctp.tal.org/api/v1');
+$fid=0;
+
+/*
 try {
 	printf("Uploading text file\n");
 	$r=$c->upload_file('upload-test-file.txt');
@@ -10,11 +13,60 @@ try {
 } catch (Exception $e) {
 	die($e->getCode().' : '.$e->getMessage());
 }
+*/
 
 try {
 	printf("Uploading image file\n");
-	$r=$c->upload_file('upload-test-image.jpg');
+	$r=$c->upload_file('upload-test-image.jpg', true);
 	print_r($r);
+	$fid=$r->fid;
+} catch (Exception $e) {
+	die($e->getCode().' : '.$e->getMessage());
+}
+
+try {
+	printf("Adding a node with uploaded image file %d\n", $fid);
+	$fields=array(
+		'body'=>array(DRUPAL_LANGUAGE_NONE=>
+			array(array(
+				'value'=>'This node has an image attached',
+				'summary'=>'Imageeeeeeeeeeeee!!!'
+			))
+		),
+		'field_image'=>array(DRUPAL_LANGUAGE_NONE=>
+			array(0=>array(
+				'fid'=>(int)$fid,
+				'alt'=>'Test image alt',
+				'title'=>'A uploaded and attached to node field image',
+				'display'=>'1',
+				'_weight'=>1
+			),
+			1=>array(
+				'fid'=>0,
+				'alt'=>'Test image alt',
+				'title'=>'A uploaded and attached to node field image',
+				'display'=>'1',
+				'_weight'=>2
+			)
+			
+			)
+		),
+		'field_weight'=>array(DRUPAL_LANGUAGE_NONE=>
+			array(array(
+				'weight'=>512
+			))
+		)
+	);
+	$c->set_debug(true);
+        $r=$c->create_node('product', 'Image upload test page', $fields);
+        print_r($r);
+} catch (Exception $e) {
+        die($e->getCode().' : '.$e->getMessage());
+}
+
+try {
+	$node=$c->retrieve_node($r->nid);
+	print_r($node);
 } catch (Exception $e) {
 	die($e->getCode().' : '.$e->getMessage());
 }
