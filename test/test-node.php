a9d6cdb02396ac4d49e5406bcf319d606a5edb89
Kaj-Michael Lang <milang@tal.org>
2015-08-19 16:12:06 +0300

    Add authentication to node test. A removal test.

--- test/test-node.php
+++ test/test-node.php
@@ -4,15 +4,34 @@ require_once('../lib/DrupalServiceAPIClient.class.php');
 
 $c=new DrupalServiceAPIClient('http://drupal.ubctp.tal.org/api/v1');
 $c->set_debug(true);
+try {
+	$c->set_auth_type(AUTH_SESSION);
+	$c->set_auth('apiuser','apiuserpassword123');
+	$c->login();
+} catch (Exception $e) {
+	die($e->getCode().' : '.$e->getMessage());
+}
+
+function load_node(DrupalServiceAPIClient $c, $nid)
+{
+try {
+	$node=$c->retrieve_node($nid);
+	print_r($node);
+} catch (Exception $e) {
+	die($e->getCode().' : '.$e->getMessage());
+}
+}
 
 try {
 	$fields=array('body'=>
-			array('und'=>
-				array(0=>array(
+			array('und'=>array(array(
 					'value'=>'This is some body text.',
 					'summary'=>'A summary text for body field'
+					)
 				)
-			)
+		),
+		'field_sku'=>array(DRUPAL_LANGUAGE_NONE=>
+			array(array('value'=>'DUMMY-TEST-SKU-123456789'))
 		)
 	);
 	$r=$c->create_node('product', 'PHP Test', $fields);
@@ -22,21 +41,28 @@ try {
 }
 
 printf("Node %d created\n", $r->nid);
+load_node($c, $r->nid);
 
+printf("Update node %d\n", $r->nid);
 try {
-	$node=$c->retrieve_node($r->nid);
+	$node=$c->update_node($r->nid, 'Updated PHP test', array());
 	print_r($node);
 } catch (Exception $e) {
 	die($e->getCode().' : '.$e->getMessage());
 }
 
-printf("Loading all nodes\n");
+load_node($c, $r->nid);
 
+printf("Loading product nodes\n");
 try {
-	$nodes=$c->index_nodes();
+	$nodes=$c->index_nodes(0, 20, null, array('type'=>'product'));
 	print_r($nodes);
 } catch (Exception $e) {
 	die($e->getCode().' : '.$e->getMessage());
 }
+
+printf("Removing test node %d\n", $r->nid);
+$node=$c->delete_node($r->nid);
+print_r($node);
 
 ?>
